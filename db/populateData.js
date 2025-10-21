bdf6d34a153cd4d26d66937ee0ced4f829cc2379
Flex58 <164533598+Flex58@users.noreply.github.com>
2025-10-21 10:49:31 +0200

    feat: finish populatedb script and populateData

---
+++ db/populateData.js
@@ -0,0 +1,41 @@
+exports.generationsQuery = `INSERT INTO generations(generation)
+                            VALUES ($1), ($2), ($3), ($4), ($5),
+                            ($6), ($7), ($8), ($9);`;
+
+exports.generationsData = [
+  "I",
+  "II",
+  "III",
+  "IV",
+  "V",
+  "VI",
+  "VII",
+  "VIII",
+  "IX",
+];
+
+exports.typeQuery = `INSERT INTO types(type)
+                    VALUES ($1), ($2), ($3), ($4), ($5), ($6), ($7), ($8),
+                            ($9), ($10), ($11), ($12), ($13), ($14), ($15), ($16),
+                            ($17), ($18);`;
+
+exports.typeData = [
+  "Normal",
+  "Fighting",
+  "Flying",
+  "Poison",
+  "Ground",
+  "Rock",
+  "Bug",
+  "Ghost",
+  "Steel",
+  "Fire",
+  "Water",
+  "Grass",
+  "Electric",
+  "Psychic",
+  "Ice",
+  "Dragon",
+  "Dark",
+  "Fairy",
+];
--- db/populatedb.js
+++ db/populatedb.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const { Client } = require("pg");
+const dbData = require("./populateData");
 
 const tableGeneration = `CREATE TABLE IF NOT EXISTS 
-generations (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, generation VARCHAR(3));`;
+generations (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, generation VARCHAR(4));`;
 
 const tableTypes = `CREATE TABLE IF NOT EXISTS 
 types (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, type VARCHAR(50));`;
@@ -12,14 +13,21 @@ pokemon(dexnr INT PRIMARY KEY, name VARCHAR(50), primary_type INT REFERENCES typ
         secondary_type INT REFERENCES types(id), generation INT REFERENCES generations(id), mega_evolution BOOL);`;
 
 const tableTrainer = `
-CREATE TYPE gender AS ENUM ('male', 'female', 'other');
+DO $$ BEGIN
+    CREATE TYPE gender AS ENUM ('male', 'female', 'other');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
 CREATE TABLE IF NOT EXISTS
 trainers (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(50),
-        gender gender, generation INT REFERENCES generations(id), pos INT, 
+        gender gender, generation INT REFERENCES generations(id), pos INT 
+        );`;
+
+const tableTeam = `CREATE TABLE IF NOT EXISTS 
+teams (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, trainer INT REFERENCES trainers(id),
         pokemon_one INT REFERENCES pokemon(dexnr), pokemon_two INT REFERENCES pokemon(dexnr),
         pokemon_three INT REFERENCES pokemon(dexnr), pokemon_four INT REFERENCES pokemon(dexnr),
-        pokemon_five INT REFERENCES pokemon(dexnr),  pokemon_six INT REFERENCES pokemon(dexnr)
-        );`;
+        pokemon_five INT REFERENCES pokemon(dexnr), pokemon_six INT REFERENCES pokemon(dexnr));`;
 
 const main = async () => {
   console.log("Seeding....");
@@ -29,6 +37,9 @@ const main = async () => {
   await client.query(tableTypes);
   await client.query(tablePokemon);
   await client.query(tableTrainer);
+  await client.query(tableTeam);
+  await client.query(dbData.generationsQuery, dbData.generationsData);
+  await client.query(dbData.typeQuery, dbData.typeData);
   await client.end();
   console.log("Done");
 };
