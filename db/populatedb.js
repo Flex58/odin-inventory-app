2b62d9d185cea9672a8f494a8886f0710c59c4de
Flex58 <164533598+Flex58@users.noreply.github.com>
2025-10-23 10:00:33 +0200

    refactor: change db structure

--- db/populatedb.js
+++ db/populatedb.js
@@ -12,22 +12,22 @@ const tablePokemon = `CREATE TABLE IF NOT EXISTS
 pokemon(dexnr INT PRIMARY KEY, name VARCHAR(50), primary_type INT REFERENCES types(id), 
         secondary_type INT REFERENCES types(id), generation INT REFERENCES generations(id), mega_evolution BOOL);`;
 
-const tableTrainer = `
-DO $$ BEGIN
-    CREATE TYPE gender AS ENUM ('male', 'female', 'other');
-EXCEPTION
-    WHEN duplicate_object THEN null;
-END $$;
-CREATE TABLE IF NOT EXISTS
-trainers (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(50),
-        gender gender, generation INT REFERENCES generations(id), pos INT 
-        );`;
-
 const tableTeam = `CREATE TABLE IF NOT EXISTS 
-teams (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, trainer INT REFERENCES trainers(id),
+        teams (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
         pokemon_one INT REFERENCES pokemon(dexnr), pokemon_two INT REFERENCES pokemon(dexnr),
         pokemon_three INT REFERENCES pokemon(dexnr), pokemon_four INT REFERENCES pokemon(dexnr),
         pokemon_five INT REFERENCES pokemon(dexnr), pokemon_six INT REFERENCES pokemon(dexnr));`;
+
+const tableTrainer = `
+                DO $$ BEGIN
+                CREATE TYPE gender AS ENUM ('male', 'female', 'other');
+                EXCEPTION
+                WHEN duplicate_object THEN null;
+                END $$;
+                CREATE TABLE IF NOT EXISTS
+                trainers (id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(50),
+                gender gender, generation INT REFERENCES generations(id), pos INT, team INT REFERENCES teams(id) 
+                );`;
 
 const main = async () => {
   console.log("Seeding....");
